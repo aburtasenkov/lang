4543150101c1075b73c4fe4257ebb96ddb8ffca7
aburtasenkov <aburtasenkov@gmail.com>
2025-02-17 20:01:13 +0100

    Added variable table

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 #include "Token_stream.hpp"
 #include "arithmetics.hpp"
 
+#include <unordered_map>
+#include <variant>
+
 int main() 
 try {
+    // creating assosiative variable table for allowed types in IPL
+    using Types = std::variant<double, std::string>;
+    std::unordered_map<std::string, Types> symbol_tbl;
+
+    // constant values
+    symbol_tbl.insert({"pi", 3.141592});
+
     Token_stream ts{std::cin};
 
     while (true) {
