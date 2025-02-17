918ce233b6d194f4fa7c9a43afae67b9c00d20f7
aburtasenkov <aburtasenkov@gmail.com>
2025-02-17 20:42:21 +0100

    Added variable table

--- main.cpp
+++ main.cpp
@@ -5,19 +5,34 @@
 #include <unordered_map>
 #include <variant>
 
+// allowed variable types in IPL interptered 
+using Types = std::variant<double, int64_t>;
+
+template <typename T>
+void define_var(std::unordered_map<std::string, Types>& symbol_tbl, const std::string& key, const T& value) {
+    std::cout << typeid(T).name() << "\t" << typeid(double).name() << "\t" << typeid(int64_t).name() << "\n";
+    if (typeid(T) == typeid(double) || typeid(T) == typeid(int64_t)) {
+        symbol_tbl[key] = value;
+        return;
+    }
+    throw std::runtime_error{"Bad Variable Definition"};
+}
+
 int main() 
 try {
-    // creating assosiative variable table for allowed types in IPL
-    using Types = std::variant<double, std::string>;
+    // assosiative variable table for allowed types
     std::unordered_map<std::string, Types> symbol_tbl;
 
     // constant values
-    symbol_tbl.insert({"pi", 3.141592});
+    define_var(symbol_tbl, "pi", 3.141592);
 
     Token_stream ts{std::cin};
 
     while (true) {
-        std::cout << ">>> " << expression(ts) << "\n";
+        std::cout << ">>> ";
+        Token t = ts.get_token();
+        if (t.name() == Token_constants::DEFINE_VAR) 
+        // std::cout << expression(ts) << "\n";
     }
 
     return 0;
