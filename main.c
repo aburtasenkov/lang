87fd2d1dd0e253f90d7d8265a009a2278cb0d451
aburtasenkov <aburtasenkov@gmail.com>
2025-03-01 20:49:12 +0100

    REORDERED ALL CODE, NOW UPDATING IT TO RUN

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include "Token.h"
-#include "arithmetics.h"
-#include "Symbol_table.h"
-#include "Exceptions.h"
+#include "source/Token.h"
+#include "source/arithmetics.h"
+#include "source/Symbol_tbl.h"
+#include "source/Exceptions.h"
+#include "source/Syntax.h"
+
+#include <malloc.h>
+#include <ctype.h>
+#include <string.h>
 
 typedef enum {
     STATUS_DEFAULT = 0,
@@ -28,43 +33,45 @@ char * get_alpha_input() {
     return name;
 }
 
-void assign(Symbol_table * tbl, char * type) {
+void assign(Symbol_tbl_int * tbl) {
     // get input and validate it
-    Token * name = get_token();
-    if (name->kind != TOKEN_TEXT) BAD_SYNTAX_ERROR();
+    Token * var_type = get_token();
+    if (var_type->kind != TOKEN_TYPE) BAD_SYNTAX_ERROR();
+
+    Token * var_name = get_token();
+    if (var_name->kind != TOKEN_TEXT) BAD_SYNTAX_ERROR();
 
     Token * equal_sign = get_token();
     if (equal_sign->kind != '=') BAD_SYNTAX_ERROR();
     
+    // currently only supporting integer values
     Token * value = get_token();
-    if (!strcmp(type, TYPE_INT))
+    if (!strcmp((char*)var_type->value, TYPE_INT))
         if (value->kind != TOKEN_NUMERAL) BAD_SYNTAX_ERROR();
-    if (!strcmp(type, TYPE_STRING))
-        if (value->kind != TOKEN_TEXT) BAD_SYNTAX_ERROR();
 
     Token * dot_dash = get_token();
     if (dot_dash->kind != ';') BAD_SYNTAX_ERROR();
 
-    tbl_insert(tbl, (char*)name->value, value);
+    tbl_add_int(tbl, (char*)var_name->value, *(int*)value);
 
-    free_token(name);
+    free_token(var_type);
+    free_token(var_name);
     free_token(equal_sign);
     free_token(value);
     free_token(dot_dash);
-
 }
 
-void compute(Symbol_table * tbl) {
+void compute(Symbol_tbl_int * tbl) {
     Token * t = get_token();
 
     switch (t->kind) {
         case TOKEN_TEXT: case TOKEN_NUMERAL:
             TOKEN_BUFFER = t;
-            printf("= %li\n", expression(tbl));
+            printf("= %i\n", expression(tbl));
             return;
         case TOKEN_TYPE:
-            // t is a type token that holds name of the type as its value
-            assign(tbl, (char*)t->value);
+            TOKEN_BUFFER = t;
+            assign(tbl);
             break;
         default: // TOKEN_EMPTY
             BAD_SYNTAX_ERROR();
@@ -86,7 +93,7 @@ int main(int argc, char ** argv) {
     }
 
     // obligatory vars
-    Symbol_table * tbl = make_symbol_table(8);
+    Symbol_tbl_int * tbl = make_symbol_tbl_int();
     
     // output all kinds of tokens
     if (status == STATUS_DEBUG) while (1) {
@@ -100,7 +107,7 @@ int main(int argc, char ** argv) {
                 if (t->value) printf("Value - %s", (char*)t->value);
                 break;
             case TOKEN_NUMERAL:
-                if (t->value) printf("Value - %li", *(__int64_t*)t->value);
+                if (t->value) printf("Value - %i", *(int*)t->value);
                 break;
         }
 
@@ -108,8 +115,7 @@ int main(int argc, char ** argv) {
         free_token(t);
     }
 
-    __int64_t pi = 3;
-    tbl_insert(tbl, "pi", make_token(get_token_kind('3'), &pi));
+    tbl_add_int(tbl, "pi", 3);
     
     // STATUS_DEFAULT
     while (1) {
